ac5e122ca344c83812fc77075e654dc54791c746
mdg0410 <levoyersteven@gmail.com>
2023-07-27 01:29:03 -0500

    Add propTypes | Add styles | Remove BookList.js

--- src/Components/BookItem.jsx
+++ src/Components/BookItem.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice'
-
 import styles from '../Styles/BookItem.module.css'
 
-// eslint-disable-next-line react/prop-types
 export default function BookItem({item_id, title, author, category}) {
   const dispatch = useDispatch();
 
@@ -12,8 +11,15 @@ export default function BookItem({item_id, title, author, category}) {
     <li className={styles.item}>
       <div className={styles.content}>
         <button onClick={() => dispatch(removeBook({item_id}))}>delete</button>
-        {title} - {author} - {category} - {item_id}
+        {title} - {author}
       </div>
     </li>
   )
 }
+
+BookItem.propTypes = {
+  item_id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+};
--- src/Components/BookLogic.jsx
+++ src/Components/BookLogic.jsx
@@ -9,19 +9,18 @@ const container = {
   flexDirection: 'column',
   alignItems: 'center',
   padding: '3vh 0',
+  gap: '2vh'
 };
 
 export default function BookLogic() {
   const books = useSelector((state) => state.books);
 
   return (
-    <>
-    <InputTodo />
     <div style={container}>
+      <InputTodo />
       {books.map((book) => {
         return <BookItem key={book.item_id} {...book} />
       })}
     </div>
-    </>
   )
 }
