5074efe108c325753556865edc64ea5731007d89
mdg0410 <levoyersteven@gmail.com>
2023-07-28 03:44:52 -0500

    setup store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./books/booksSlice";
 import categoriesReducer from "./categories/categoriesSlice";
 
-const rootReducer = combineReducers({
-  books: booksReducer,
-  categories: categoriesReducer,
-});
-
 const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools in development
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
 });
 
 export default store;
